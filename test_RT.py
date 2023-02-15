d8e73760c1acd5dbba997a298d37483cf57b7d1e
AleksandrAdam <116949875+AleksandrAdam@users.noreply.github.com>
2023-02-15 23:00:03 +0300

    Update test_RT.py

--- test_RT.py
+++ test_RT.py
@@ -8,7 +8,7 @@ from selenium.webdriver.support import expected_conditions as EC
 ### 1. Для тест-кейса TC-rt-001. Проверка успешного входа в аккаунт по логину.
 
 def test_001(selenium):
-    form = AuthForm(selenium)
+    form = Auth(selenium)
 
     # ввод телефона
     form.username.send_keys(valid_login)
@@ -20,7 +20,7 @@ def test_001(selenium):
     
  ### 2. Для тест-кейса TC-rt-001. Проверка возникновения ошибки авторизации при введении неверного пароля.
 def test_001_wrong(selenium):
-    form = AuthForm(selenium)
+    form = Auth(selenium)
 
     # ввод телефона
     form.username.send_keys(valid_login)
@@ -36,7 +36,7 @@ def test_001_wrong(selenium):
 ### 3. Для тест-кейса TC-rt-003. Проверка успешного входа в аккаунт по номеру телефона.
 
 def test_003(selenium):
-    form = AuthForm(selenium)
+    form = Auth(selenium)
 
     # ввод телефона
     form.username.send_keys(valid_phone)
@@ -48,7 +48,7 @@ def test_003(selenium):
 
 ### 4. Для тест-кейса TC-rt-003. Проверка возникновения ошибки авторизации при введении неверного пароля.
 def test_003_wrong(selenium):
-    form = AuthForm(selenium)
+    form = Auth(selenium)
 
     # ввод телефона
     form.username.send_keys(valid_phone)
@@ -62,7 +62,7 @@ def test_003_wrong(selenium):
 ### 5. Для тест-кейса TC-rt-004. Проверка успешного входа в аккаунт по электронной почте.
 
 def test_004(selenium):
-    form = AuthForm(selenium)
+    form = Auth(selenium)
 
     # ввод телефона
     form.username.send_keys(valid_email)
@@ -74,7 +74,7 @@ def test_004(selenium):
     
 ### 6. Для тест-кейса TC-rt-004. Проверка возникновения ошибки авторизации при введении неверного пароля.
 def test_004_wrong(selenium):
-    form = AuthForm(selenium)
+    form = Auth(selenium)
 
     # ввод телефона
     form.username.send_keys(valid_email)
@@ -88,7 +88,7 @@ def test_004_wrong(selenium):
  ### 7. Для тест-кейса TC-rt-005. Проверка успешного входа в аккаунт по номеру лицевого счёта.
 
 def test_005(selenium):
-    form = AuthForm(selenium)
+    form = Auth(selenium)
 
     # ввод телефона
     form.username.send_keys(valid_personalnum)
@@ -100,7 +100,7 @@ def test_005(selenium):
     
 ### 8. Для тест-кейса TC-rt-005. Проверка возникновения ошибки авторизации при введении неверного пароля.
 def test_005_wrong(selenium):
-    form = AuthForm(selenium)
+    form = Auth(selenium)
 
     # ввод телефона
     form.username.send_keys(valid_personalnum)
@@ -114,14 +114,14 @@ def test_005_wrong(selenium):
  
 ### 9. Для тест-кейса TC-rt-002. Проверка автоматической смены таба ввода авторизации.
 def test_002(selenium):
-    form = AuthForm(selenium)
+    form = Auth(selenium)
 
     # ввод логина
     form.username.send_keys(valid_login)
     form.password.send_keys('_')
     sleep(5)
 
-    assert form.placeholder.text == 'Логин'
+    assert form.place.text == 'Логин'
 
     # Удаление введенных данных
     form.username.send_keys(Keys.CONTROL, 'a')
@@ -132,7 +132,7 @@ def test_002(selenium):
     form.password.send_keys('_')
     sleep(5)
 
-    assert form.placeholder.text == 'Электронная почта'
+    assert form.place.text == 'Электронная почта'
 
     # Удаление введенных данных
     form.username.send_keys(Keys.CONTROL, 'a')
@@ -143,18 +143,18 @@ def test_002(selenium):
     form.password.send_keys('_')
     sleep(5)
 
-    assert form.placeholder.text == 'Лицевой счёт'
+    assert form.place.text == 'Лицевой счёт'
     
     # ввод телефона
     form.username.send_keys(valid_phone)
     form.password.send_keys('_')
     sleep(5)
 
-    assert form.placeholder.text == 'Мобильный телефон'
+    assert form.place.text == 'Мобильный телефон'
  
 ### 10. Для тест-кейса TC-rt-006. Проверка перехода в форму регистрации
 def test_006(selenium):
-    form = AuthForm(selenium)
+    form = Auth(selenium)
 
     # "Зарегистрироваться"
     form.register.click()
@@ -166,10 +166,10 @@ def test_006(selenium):
 
 ### 11. Для тест-кейса TC-rt-007. Проверка перехода в форму восстановления
 def test_007(selenium):
-    form = AuthForm(selenium)
+    form = Auth(selenium)
 
     # "Забыл пароль"
-    form.forgot.click()
+    form.forgotpass.click()
     sleep(5)
 
     final_h = form.driver.find_element(By.XPATH, '//*[@id="page-right"]/div/div/h1')
@@ -178,7 +178,7 @@ def test_007(selenium):
 
 ### 12. Для тест-кейса TC-rt-011. Проверка получения временного кода на телефон
 def test_011_phone(selenium):
-    form = CodeForm(selenium)
+    form = Code(selenium)
 
     # ввод номера телефона
     form.address.send_keys(valid_phone)
@@ -191,7 +191,7 @@ def test_011_phone(selenium):
 
     ### 13. Для тест-кейса TC-rt-011. Проверка получения временного кода на электронную почту
 def test_011_email(selenium):
-    form = CodeForm(selenium)
+    form = Code(selenium)
 
     # ввод номера телефона
     form.address.send_keys(valid_email)
